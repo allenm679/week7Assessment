5c5d086b1c71393c05a1f02a861df5dc587fd71a
Allen Mounivong <allenmounivong@gmail.com>
2022-05-06 12:20:30 -0500

    first commit

---
+++ cs/questions.js
@@ -0,0 +1,58 @@
+//Sum Zero
+const addToZero = (arr) => {
+    for (let i = 0; i < arr.length; i++){
+    for(let j = i + 1; j < arr.length; j++){if(arr[i] + arr[j] === 0){
+            return true;
+        }
+    }
+  }
+  return false;
+}
+
+// runtime = O(n^2)
+
+//Unique characters
+
+const hasUniqueCharacters = (string) => {
+    for (let i = 0; i < string.length; i++){
+        for (let j = i + 1; j < string.length; j++){
+            if(string[i] === string[j]){
+                return false;
+            }
+        }
+     }
+     return true;
+}
+
+//Runtime = O(n^2)
+
+//Panagram 
+
+const isPangram = (str) => {
+    for(let i = 0; i < alphabet.length; i++){
+        for(let j = 0; j < str.length; j++){
+            if(alphabet[i] == str[j]){
+                alphabet.splice(alphabet[j], 1)
+                console.log(alphabet)
+                i--;
+            }
+        }
+    }
+    if(alphabet.length == 0){
+        return true;
+    }
+    return false;
+}
+//Runtime = O(n^2)
+
+//longest word
+const findLongestWord = (arr) => {
+    let longestWord = arr[0].length;
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i].length > longestWord){
+            longestWord = arr[i].length;
+        }
+    }
+    return longestWord
+}
+//Runtime O(n)
